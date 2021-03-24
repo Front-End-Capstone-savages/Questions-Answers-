416af833d6006fe4de52d474fde9bd1e25095c06
BaslaHassine <turkibaslahassine@gmail.com>
2021-03-24 10:54:34 +0100

    create a search bar

---
+++ service/client/components/Search.jsx
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+import axios from "axios";
+import token from "../config/config.js"
+
+export default class Search extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      questions: [],
+      search: "",
+    };
+  }
+
+  componentDidMount() {
+    axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hrnyc/qa/questions/?product_id=${11002}`, {
+      headers: {
+        'Authorization': `${token}`
+      }
+    })
+    .then((res) => {
+      this.setState({questions: res.data.results})
+    })
+    .catch((error) => {
+      console.error(error)
+    })
+  }
+
+  render() {
+    console.log(this.state);
+
+    return (
+      <div className="container">
+        <div className="row">
+          <div id="custom-search-input">
+            <div className="input-group col-md-12">
+              <input
+                type="text"
+                className="  search-query form-control"
+                placeholder="Have a question? Search for answers..."
+              />
+              <span className="input-group-btn">
+                <button className="btn btn-danger btn-search" type="button">
+                  <i className="bi bi-search">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      className="bi bi-search"
+                      viewBox="0 0 16 16" >
+                      <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+                    </svg>
+                  </i>
+                </button>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
---
+++ service/client/config/config.js
@@ -0,0 +1,4 @@
+
+const token = "132d11734b4f6e7902ae8a6fb6ae30d4cb16285e" 
+
+export default token;
