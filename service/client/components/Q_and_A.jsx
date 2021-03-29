990583e7361a19ea32402498443cfcb4fea733b2
BaslaHassine <turkibaslahassine@gmail.com>
2021-03-29 17:29:07 +0100

    almost done

--- service/client/components/Q_and_A.jsx
+++ service/client/components/Q_and_A.jsx
@@ -10,21 +10,30 @@ export default class Q_and_A extends Component {
     this.state = {
       product_id: 11050,
       quests_answers: [],
+      ///////////////
       displayed_quests: 2,
       displayed_answers: 2,
+      /////////////
       likeQuest: [],
       likeAnswers: [],
+      /////////////
       authoAdd_Q: true, 
       body_Q: "",
       name_Q: "",
       email_Q: "",
-      product_id: null,
+      /////////////
+      authoAdd_A: true,
+      body_A: "",
+      name_Q: "",
+      email_A: "",
+      imag_A: "",
     };
 
     this.updateQuestion_helpfulness = this.updateQuestion_helpfulness.bind(this);
     this.updateAnswer_helpful = this.updateAnswer_helpful.bind(this);
     this.updateQuestion_Reported = this.updateQuestion_Reported.bind(this);
-    // this.create_Quest = this.create_Quest.bind(this);
+    this.post_Question = this.post_Question.bind(this);
+    this.post_Answer = this.post_Answer.bind(this);
   }
 
   componentDidMount() {
@@ -66,23 +75,40 @@ export default class Q_and_A extends Component {
     }
   }
 
-  updateQuestion_Reported(question_id, report) {
-    console.log(question_id, report);
+  updateQuestion_Reported(question_id, report, index) {
+    const array_R = this.state.quests_answers;
+    array_R[index].reported = report;
+    console.log(question_id, report, index);
     axios.put(`https://app-hrsei-api.herokuapp.com/api/fec2/hrnyc/qa/questions/${question_id}/report`,report,
       {headers: { Authorization: `${token}` },})
       .then((res) => {
         console.log("question_reported", res);
+        this.setState({quests_answers: array_R})
       }).catch((err) => console.log(err));
   }
 
-  // create_Quest() {
-  //   axios.put(`https://app-hrsei-api.herokuapp.com/api/fec2/hrnyc/qa/questions/`,this.state.create_Q,
-  //     {headers: { Authorization: `${token}` },})
-  //     .then((res) => {
-  //       console.log("question_reported", res);
-  //     }).catch((err) => console.log(err));
-  // }
+  post_Question(e) {
+    e.preventDefault();
+    const {body_Q, name_Q, email_Q, product_id} = this.state;
+    axios.post(`https://app-hrsei-api.herokuapp.com/api/fec2/hrnyc/qa/questions/`,{body: body_Q, name: name_Q, email: email_Q, imag_A, product_id},
+      {headers: { Authorization: `${token}` },})
+      .then((res) => {
+        console.log("Post Question", res);
+        this.setState({authoAdd_Q: !this.state.authoAdd_Q})
+      }).catch((err) => console.log(err));
+  }
 
+  post_Answer(e, question_id) {
+    e.preventDefault();
+    const {body_A, name_A, email_A, imag_A} = this.state;
+    console.log(question_id, body_A, name_A, email_A, imag_A);
+    axios.post(`https://app-hrsei-api.herokuapp.com/api/fec2/hrnyc/qa/questions/${question_id}/answers`,{body: body_A, name: name_A, email: email_A, imag_A},
+      {headers: { Authorization: `${token}` },})
+      .then((res) => {
+        console.log("Post Answer", res);
+        this.setState({authoAdd_A: !this.state.authoAdd_A})
+      }).catch((err) => console.log(err));
+  }
 
   render() {
     const questions = this.state.quests_answers.filter(
@@ -113,7 +139,8 @@ export default class Q_and_A extends Component {
                         <u type="button" onClick={() => this.updateAnswer_helpful(answer.id, answer.helpfulness+1, index, )}>
                           <strong>Yes</strong></u>(
                         {answer.helpfulness}) |{" "}
-                        <u type="button" onClick={() => this.updateQuestion_Reported(question.question_id, !question.reported)}><strong>Report</strong></u>
+                        <u type="button" onClick={() => this.updateQuestion_Reported(question.question_id, !question.reported, index)}>
+                          <strong>Report</strong></u>
                       </small>
                     </pre>
                   </div>
@@ -131,11 +158,11 @@ export default class Q_and_A extends Component {
               </strong>
               </p>
             </div>
+
             <div className="col-4 addAnswerForQuest">
               <pre>
                 <small>
-                  <strong>Helpful?</strong>{" "}
-                  
+                  <strong>Helpful?</strong>{" "}  
                   <u type="button"
                     id={index}
                     onClick={() =>
@@ -149,10 +176,31 @@ export default class Q_and_A extends Component {
                     <strong>Yes</strong>
                   </u>
                   ({question.question_helpfulness}) |{" "}
-                  <u type="button" onClick={() => console.log("click")}><strong>Add Answer</strong></u>
+                  <u type="button" onClick={() => {this.setState({authoAdd_A: !this.state.authoAdd_A}), console.log(this.state.authoAdd_A)}}>
+                    <strong>Add Answer</strong></u>
                 </small>
               </pre>
             </div>
+            {!this.state.authoAdd_A ?
+          <div className="create container">
+          <div className="create-editor container">
+            <form>
+              <input  className="create-input" type="text"  placeholder="Enter your Name"
+                    onChange={(e)=>{this.setState({name_A: e.target.value})}}/>
+              <input className="create-input" type="email"  placeholder="Enter your E-mail"
+                    onChange={(e)=>{this.setState({email_A: e.target.value})}}/>
+              <input className="create-input" type="text"  placeholder="Image URL"
+                     onChange={(e)=>{this.setState({imag_A: e.target.value})}}/>
+              <textarea className="create-body-textarea"  placeholder="Answer Body"
+                    onChange={(e)=>{this.setState({body_A: e.target.value})}}/>
+              <button className="create-submit-button" 
+                        onClick={(e)=>this.post_Answer(e, question.question_id)}>
+                  <strong>POST ANSWER</strong>
+                  </button>
+            </form>
+            </div></div>
+         : ""}
+
           </article>
         ))}
         {this.state.quests_answers.length - this.state.displayed_quests > 0 ? (
@@ -180,28 +228,28 @@ export default class Q_and_A extends Component {
             <strong>LESS QUESTION</strong>
           </button>
         )}
-        <button className="add_question_button" type="button" onClick={()=>this.state.authoAdd_Q = !this.state.authoAdd_Q}><strong>ADD A QUESTION  +</strong></button>
+        <button className="add_question_button" type="button" onClick={()=>this.setState({authoAdd_Q: !this.state.authoAdd_Q})}><strong>ADD A QUESTION  +</strong></button>
         <div>
           {!this.state.authoAdd_Q ? 
-           <div className="create">
-  <div className="create-editor">
-    <form>
-      <input  className="create-input" type="text"  placeholder="Enter your Name"
-            onChange={(e)=>{this.setState({name_Q: e.target.value})}}>
-      </input>
-      <input className="create-input" type="email"  placeholder="Enter your E-mail"
-            onChange={(e)=>{this.setState({email_Q: e.target.value})}}>
-      </input>
-      <textarea className="create-body-textarea"  placeholder="Post Body"
-            onChange={(e)=>{this.setState({body: e.target.value})}}>
-      </textarea>
-      <button className="create-submit-button" type="submit"
-                onClick={this.submit}>
-          Save post
-          </button>
-    </form>
-  </div>
-</div> : ""}
+           <div className="create container">
+            <div className="create-editor container">
+              <form>
+                <input  className="create-input" type="text"  placeholder="Enter your Name"
+                      onChange={(e)=>{this.setState({name_Q: e.target.value})}}>
+                </input>
+                <input className="create-input" type="email"  placeholder="Enter your E-mail"
+                      onChange={(e)=>{this.setState({email_Q: e.target.value})}}>
+                </input>
+                <textarea className="create-body-textarea"  placeholder="Question Body"
+                      onChange={(e)=>{this.setState({body_Q: e.target.value})}}>
+                </textarea>
+                <button className="create-submit-button" 
+                          onClick={(e)=>this.post_Question(e)}>
+                    <strong>POST QUESTION</strong>
+                    </button>
+              </form>
+            </div>
+          </div>:""}
         </div>
       </div>
     );
